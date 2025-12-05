674ce8beb9bf71195b0d215727bebc0a226ed6c6
Chamu-Rajasekera <chrajase@microsoft.com>
2025-12-05 12:55:50 -0800

    remove concurrently, pac code run, use plugin

--- templates/vite/vite.config.ts
+++ templates/vite/vite.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import * as path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
+import { powerApps, powerAppsCorsOrigins } from "./plugins/powerApps";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "./",
   server: {
-    host: "::",
-    port: 3000,
+    cors: {
+      // allow apps.powerapps.com to access dev server
+      origin: powerAppsCorsOrigins,
+    },
   },
-  plugins: [react()],
+  plugins: [react(), powerApps()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
