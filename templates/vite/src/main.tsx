c5fb5cdef2f91eccf4c4bb2a7c9f7e6ce5ffdc51
Chamu-Rajasekera <chrajase@microsoft.com>
2025-12-04 16:21:34 -0800

    customizations

--- templates/vite/src/main.tsx
+++ templates/vite/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import PowerProvider from './PowerProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PowerProvider>
+      <App />
+    </PowerProvider>
   </StrictMode>,
 )
