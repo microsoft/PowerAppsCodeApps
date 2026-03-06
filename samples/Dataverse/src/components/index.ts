33b506492c36ce876b30c7778b5475b885b944fb
madhumach <94madhu94@gmail.com>
2026-03-05 20:30:11 -0800

    add Dataverse upload feature

--- samples/Dataverse/src/components/index.ts
+++ samples/Dataverse/src/components/index.ts
@@ -8,6 +8,10 @@ export { ErrorMessage } from './ErrorMessage';
 export { ContactCard } from './ContactCard';
 export { ContactList } from './ContactList';
 export { ContactForm } from './ContactForm';
+export { AccountCard } from './AccountCard';
+export { AccountList } from './AccountList';
+export { AccountForm } from './AccountForm';
 export { Footer } from './Footer';
 
 export type { ContactFormData } from './ContactForm';
+export type { AccountFormData } from './AccountForm';
